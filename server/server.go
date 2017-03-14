f3cbce7a5643b083e4f984ab7ed96398740778c2
Chris Walker <thatamazingweb@gmail.com>
2017-03-14 14:59:11 +0000

    Add basic search handler, which returns godoc results as plain text

--- server/server.go
+++ server/server.go
@@ -6,12 +6,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 var (
 	port    = flag.String("p", "8000", "Port for the doc server to listen on")
 	docRoot = flag.String("docroot", "", "Top-level directory to serve static content from")
 )
+
+func search(w http.ResponseWriter, r *http.Request) {
+	// Get query
+	q := r.URL.Query().Get("q")
+	fmt.Println(q)
+	// Shell out
+	args := strings.Split(q, " ")
+	output, err := exec.Command("godoc", args...).Output()
+	if err != nil {
+		log.Fatal(err)
+		// Pretty bad if we can't run it, but we'll check it's presence along
+		// with a correct $GOPATH etc when refactoring
+		os.Exit(1)
+	}
+	fmt.Println(string(output))
+	// Write response to client
+	fmt.Fprintf(w, string(output))
+}
 
 func main() {
 	flag.Parse()
@@ -28,6 +48,8 @@ func main() {
 	http.Handle("/css", fs)
 	http.Handle("/scripts", fs)
 	http.Handle("/images", fs)
+
+	http.HandleFunc("/search", search)
 
 	// Search requests handled as normal
 	http.ListenAndServe(":"+*port, nil)
